1b6d0917a9d67567ca52406eb9f33bd72591a515
RakanSalama <superrakan@hotmail.com>
2023-03-25 18:54:36 +0300

    S

---
+++ Lab1/App.java
@@ -0,0 +1,21 @@
+package Lab1;
+
+import java.time.LocalDate;
+
+public class App {
+
+    public static void main(String[] args) {
+        Product[] products = {
+                new Product(1234, 9.99, "water"),
+                new FoodProduct(3452, 10.0, "Cheddar Cheese", LocalDate.parse("2022-06-07")),
+                new ElectricProduct(4875, 30.0, "Extension cord", "220v")
+        };
+        for (Product p : products) {
+            System.out.println(p);
+            if (p instanceof ElectricProduct) {
+                ElectricProduct ep = (ElectricProduct) p;
+                ep.addToShoppingCart();
+            }
+        }
+    }
+}
---
+++ Lab1/ProductTest.java
@@ -0,0 +1,17 @@
+package Lab1;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class ProductTest 
+{
+  @Test
+  public void shouldCountQuantity()
+  {
+    Product p1 = new Product(6745, 5.50, "Penne Pasta");
+    Product p2 = new Product(8853, 6.50, "Spaghetti Pasta");
+    Product p3 = new Product(2106, 4.50, "Linguine Pasta");
+    assertEquals(p3.getTotalQuantity(), 2);
+  }
+
+}
